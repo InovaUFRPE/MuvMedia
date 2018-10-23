0fd5c596c91b8bc119f4eb23a0554695e74b15d6
Italo Sousa <italusousa@gmail.com>
2018-10-23 08:15:23 -0400

    versao 1.2.0

--- app/muver/muver.router.js
+++ app/muver/muver.router.js
@@ -8,5 +8,6 @@ router.post('/', controller.createMuver);
 router.get('/:id', controller.loadMuver);
 router.put('/:id', controller.updateMuver);
 router.delete('/:id', controller.removeMuver);
+router.get('/user/:id', controller.loadMuverByUser);
 
 module.exports = router;
