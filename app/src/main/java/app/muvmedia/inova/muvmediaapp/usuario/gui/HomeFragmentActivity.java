2fc03b0ad9eb669529855d740ca041e84ff19ff6
AndersonRodr <andersonr990@gmail.com>
2018-10-24 18:16:00 -0300

    Mudança na implementação do maps.

--- app/src/main/java/app/muvmedia/inova/muvmediaapp/usuario/gui/HomeFragmentActivity.java
+++ app/src/main/java/app/muvmedia/inova/muvmediaapp/usuario/gui/HomeFragmentActivity.java
@@ -1,57 +1,188 @@
 package app.muvmedia.inova.muvmediaapp.usuario.gui;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import app.muvmedia.inova.muvmediaapp.R;
 
-public class HomeFragmentActivity extends Fragment implements OnMapReadyCallback {
 
-    GoogleMap map;
+public class HomeFragmentActivity extends Fragment implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private GoogleMap map;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest pedirLocal;
+    private Location ultimaLocalizacao;
+    private Marker localAtual;
+    private static final int Codigo_Local_User = 99;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+
         return v;
     }
 
-
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    //
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         chamarMapa();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            checkPermissaoLocalUser();
+        }
+
     }
 
-    private void chamarMapa(){
-        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.mapinha1);
+    private void chamarMapa() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapinha1);
         mapFragment.getMapAsync(this);
     }
 
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map=googleMap;
-        LatLng latLng = new LatLng(40.6643, -73.9385);
+        map = googleMap;
 
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            buildGoogleApiClient();
+            map.setMyLocationEnabled(true);
+        }
+    }
+
+    //
+    public boolean checkPermissaoLocalUser(){
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) getContext(), Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Codigo_Local_User);
+            }
+            else {
+                ActivityCompat.requestPermissions((Activity) getContext(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Codigo_Local_User);
+            }
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case Codigo_Local_User:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        if (googleApiClient == null){
+                            buildGoogleApiClient();
+                        }
+                        map.setMyLocationEnabled(true);
+                    }
+                }
+                else {
+                    Toast.makeText(getActivity(), "Permissão negada", Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
+
+    }
+
+    protected synchronized void buildGoogleApiClient(){
+        googleApiClient = new GoogleApiClient.Builder(getContext()).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        googleApiClient.connect();
+    }
+
+
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        ultimaLocalizacao = location;
+
+        if (localAtual != null){
+            localAtual.remove();
+        }
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         MarkerOptions marker = new MarkerOptions();
         marker.position(latLng);
-        marker.title("Em Nova York");
-        map.addMarker(marker);
+        marker.title("Você está aqui!");
+        marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        localAtual = map.addMarker(marker);
         map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        map.animateCamera(CameraUpdateFactory.zoomBy(12));
+
+        if (googleApiClient != null){
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, (LocationListener) getContext());
+        }
+
+        String lat = String.valueOf(ultimaLocalizacao.getLatitude());
+        String lon = String.valueOf(ultimaLocalizacao.getLongitude());
+
+        Toast.makeText(getContext(), lat+"\n"+lon, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        pedirLocal = new LocationRequest();
+        pedirLocal.setInterval(1100);
+        pedirLocal.setFastestInterval(1100);
+        pedirLocal.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, pedirLocal, this);
+        }
+
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
 }
