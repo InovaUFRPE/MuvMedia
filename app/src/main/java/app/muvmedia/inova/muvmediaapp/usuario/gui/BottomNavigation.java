63d3a65bee410bb4edeb680f2b4cf966b7f7ce7a
AndersonRodr <andersonr990@gmail.com>
2018-12-01 16:50:26 -0200

    Localização: enviada a cada 1min e atualizada a cada 5s
    Falta converter o toten json recebido em uma lista de totens

--- app/src/main/java/app/muvmedia/inova/muvmediaapp/usuario/gui/BottomNavigation.java
+++ app/src/main/java/app/muvmedia/inova/muvmediaapp/usuario/gui/BottomNavigation.java
@@ -6,13 +6,13 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
@@ -22,10 +22,22 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Chronometer;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 import app.muvmedia.inova.muvmediaapp.R;
+import app.muvmedia.inova.muvmediaapp.cupom.dominio.Toten;
+import app.muvmedia.inova.muvmediaapp.infra.HttpConnection;
 import app.muvmedia.inova.muvmediaapp.infra.ServicoDownload;
 import app.muvmedia.inova.muvmediaapp.infra.Sessao;
 
@@ -35,15 +47,23 @@ public class BottomNavigation extends AppCompatActivity {
     private int tela ;
     private boolean enviar = true;
 
+
+
+    private Location minhaLocalizacao2;
+    private List<Toten> locaisToten = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
+        getLocalizacaoAparelho2();
         chamarMapaHome();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(navListener);
 
         if (isOnline()){
+            atualizarLocalizacao();
             iniciarCont();
         }
     }
@@ -54,12 +74,13 @@ public class BottomNavigation extends AppCompatActivity {
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     switch (item.getItemId()){
                         case R.id.navigation_home2:
-                            HomeTeste.setMinhaLocalizacao();
+//                            HomeTeste.setMinhaLocalizacao();
 //                            HomeTeste.cont = false;
                             if (tela != 1){
                                 HomeTeste homeTeste = new HomeTeste();
                                 FragmentManager manager1 = getSupportFragmentManager();
                                 manager1.beginTransaction().replace(R.id.fragment_container, homeTeste).commit();
+                                getLocalizacaoAparelho2();
                                 tela = 1;
                             }
                             else{Log.e("E","Fragmento já Aberto"); return false;}break;
@@ -67,12 +88,14 @@ public class BottomNavigation extends AppCompatActivity {
                         case R.id.navigation_anunciar:
                             if (tela != 2) {
                                 selectedFragment = new AnuncioFragmentActivity();
+                                getLocalizacaoAparelho2();
                                 tela = 2;
 //                                HomeTeste.setCont();
                             }
                             break;
                         case R.id.navigation_perfil:
                             selectedFragment = new PerfilFragmentActivity();
+                            getLocalizacaoAparelho2();
 //                            HomeTeste.setCont();
                             break;
                     }
@@ -85,25 +108,33 @@ public class BottomNavigation extends AppCompatActivity {
         HomeTeste homeTeste = new HomeTeste();
         FragmentManager manager1 = getSupportFragmentManager();
         manager1.beginTransaction().replace(R.id.fragment_container, homeTeste).commit();
+        getLocalizacaoAparelho2();
         tela = 1;
     }
 
     private void iniciarCont(){
         final Handler handler = new Handler();
-        Log.i("Contador", "Iniciou contador");
         Runnable runnable = new Runnable() {
             @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void run() {
-                handler.postDelayed(this, 10000);
-                if (HomeTeste.getMinhaLocalizacao() != null) {
+                handler.postDelayed(this, 60000);
+                if (minhaLocalizacao2 != null) {
+//                    Log.i("Resposta", minhaLocalizacao2.getLatitude() + " " + minhaLocalizacao2.getLongitude());
                     try {
                         enviarLocalizacao();
+
+                        Log.i("ListaToten Size", String.valueOf(locaisToten.size()));
+                        if (locaisToten.size() != 0){
+                            Toast.makeText(BottomNavigation.this, String.valueOf(locaisToten.get(0).getLocation().get(0).getLatitude() + " "
+                                    + locaisToten.get(0).getLocation().get(0).getLongitude()), Toast.LENGTH_SHORT).show();
+                        }
+
+
                     } catch (InterruptedException e) {
                         Toast.makeText(BottomNavigation.this, "Erro inesperado", Toast.LENGTH_SHORT).show();
                         e.printStackTrace();
                     }
-                    Log.i("Contador", "Lat/Lon: " + HomeTeste.getMinhaLocalizacao().getLatitude() + " " + HomeTeste.getMinhaLocalizacao().getLongitude());
                 }
             }
         };
@@ -111,31 +142,69 @@ public class BottomNavigation extends AppCompatActivity {
     }
 
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    private void enviarLocalizacao() throws InterruptedException {
-        Gson gson = new Gson();
-//        String idSailor = gson.toJson(Sessao.instance.getSailor().get_id());
-//        String location = gson.toJson(HomeTeste.getMinhaLocalizacao());
-        callServerPostLocalizacao();
+    public void getLocalizacaoAparelho2(){
+        FusedLocationProviderClient provedorLocalizacaoCliente;
 
-        if (Sessao.instance.getResposta().contains("Sucess")){
-            if (enviar){
-                enviarNotificacao();
-//                notinha();
-                receber();
-            }
+        provedorLocalizacaoCliente = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+        try{
+                final Task location = provedorLocalizacaoCliente.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+                            Location currentLocation = (Location) task.getResult();
+                            if (currentLocation == null){
+                                Toast.makeText(getApplicationContext(), "Ative o GPS Porfavor", Toast.LENGTH_SHORT).show();
+                                Log.e("E","Cliente desligou GPS desligado");
+                            }
+                            else{
+                                minhaLocalizacao2 = currentLocation;
+                            }
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Não podemos encontrar sua localização atual", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+        }catch (SecurityException e){
+            e.getMessage();
         }
-        enviar=false;
     }
 
-    //    private void callServerPostLocalizacao(final String location, final String idSailor) throws InterruptedException {
-    private void callServerPostLocalizacao() throws InterruptedException {
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void enviarLocalizacao() throws InterruptedException {
+        callServerPostLocalizacao(minhaLocalizacao2);
+
+        if (isOnline()){
+//                enviarNotificacao();
+//                notinha();
+//                receber();
+        }
+//        enviar=false;
+    }
+
+    private void callServerPostLocalizacao(final Location location) throws InterruptedException {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                Sessao.instance.setResposta("Haha Sucess");
-//                Sessao.instance.setResposta(HttpConnection.post("https://capitao-api.herokuapp.com/public/register",location, idSailor));//Setar URL
+                //testando localização falsa
+//                Sessao.instance.setResposta(HttpConnection.get("https://capitao-api.herokuapp.com/totens/nearby?lat="+ -8.058969 + "&lon=" + -34.935671));
+
+                //testando localização verdadeira
+                Sessao.instance.setResposta(HttpConnection.get("https://capitao-api.herokuapp.com/totens/nearby?lat="+ location.getLatitude() +"&lon=" + location.getLongitude()));
+
+                Type type = new TypeToken<List<Toten>>(){}.getType();
+                Gson gson = new Gson();
+                if (!Sessao.instance.getResposta().contains("location")) {
+                    Log.i("Resposta Json vazio", Sessao.instance.getResposta());
+                }
+                else{
+                    Log.i("Resposta Json correto", Sessao.instance.getResposta());
+                    locaisToten = gson.fromJson(Sessao.instance.getResposta(), type);
+                    Toast.makeText(BottomNavigation.this, String.valueOf(locaisToten.get(0)), Toast.LENGTH_SHORT).show();
+                }
             }
         });
         thread.start();
@@ -176,7 +245,7 @@ public class BottomNavigation extends AppCompatActivity {
 
     private void receber(){
         getIntent().getStringExtra(String.valueOf(R.drawable.logo));
-        Sessao.instance.setCodigo("");
+//        Sessao.instance.setCodigo("");
     }
 
 
@@ -216,5 +285,29 @@ public class BottomNavigation extends AppCompatActivity {
 
         }
     }
+
+
+
+
+    private void atualizarLocalizacao(){
+        final Handler handler = new Handler();
+        Log.i("Contador", "Iniciou contador");
+        Runnable runnable = new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void run() {
+                handler.postDelayed(this, 5000);
+                if (minhaLocalizacao2 != null) {
+                    getLocalizacaoAparelho2();
+                    Log.i("Contador", "Lat/Lon: " + minhaLocalizacao2.getLatitude() + " " + minhaLocalizacao2.getLongitude());
+                }
+            }
+        };
+        handler.postDelayed(runnable, 0);
+    }
+
+
+
+
 
 }
